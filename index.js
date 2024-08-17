0d143dfcf51edf2ff803b02595ae584e16c2b407
Sohana-Karniz-Mimmi <sohanasheikhmimmi@gmail.com>
2024-08-17 11:46:20 +0600

    Get all product data from db

--- index.js
+++ index.js
@@ -10,16 +10,18 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
+    origin: ["http://localhost:5173", "http://localhost:5174"],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(cookieParser());
 
+
+
+// const uri = `mongodb://localhost:27017`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2xcjib6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,9 +32,38 @@ const client = new MongoClient(uri, {
   },
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    const productCollection = client.db("ZenMarket").collection("products");
+
+    /*****************Start*****************************/
+    // Get all product data from db
+    app.get(`/products`, async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+   
+    /*******************end***************************** */
+
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Travel Guide Server is running");
+  res.send("ZenMarket Server is running");
 });
 
 app.listen(port, () => {
